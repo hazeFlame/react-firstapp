85f05c899491189f61db6f065e78395380af79f8
hazeFlame <1139209412@qq.com>
2017-06-01 18:05:05 +0800

    ~

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import App from './App';
+import routes from './router';
 import registerServiceWorker from './registerServiceWorker';
 import './assets/css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const RouterTree = () => {
+  return <Route routes={routes}/>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
 registerServiceWorker();
--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,15 @@
 import React from 'react'
-import { Router, Route, Link } from 'react-router'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import App from '../App.js'
+import Home from '../components/home/home.js'
+import About from '../components/about/about.js'
+
+ const routes = (
+   <Router>
+       <Route exact path="/" component={Home}/>
+       <Route path="/home" component={Home}/>
+       <Route path="/about" component={About}/>
+   </Router>
+)
+export default routes
